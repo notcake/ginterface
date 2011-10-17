04171babf9457c3a7457e3e0d33c0714db0d8945
!cake <cakenotfound@googlemail.com>
2011-10-17 21:09:25 +0000

    [GInterface] Update

--- lua/ginterface/interfacedefinition.lua
+++ lua/ginterface/interfacedefinition.lua
@@ -1,29 +1,60 @@
 local self = {}
 GInterface.InterfaceDefinition = GInterface.MakeConstructor (self)
 
-function self:ctor (name)
+function self:ctor (name, base)
 	self.Name = name
+	self.Base = base
+	
+	self.Events = {}
 	self.Methods = {}
+	
+	self.Singleton = nil
+end
+
+function self:AddEvent (name)
+	local callback = GInterface.CallbackMethod (name, self)
+	callback:AddArgument (self:GetName (), self:GetName ():lower ())	:End ()
+	
+	self.Events [name] = callback	
+	return callback
 end
 
 function self:AddMethod (name)
-	self.Methods [name] = true
+	self.Methods [name] = GInterface.Method (name)
+	return self.Methods [name]
 end
 
 function self:CheckProvider (provider)
-	local missing = {}
+	local valid, missing = true, nil
+	if self.Base then
+		local base = GInterface.GetDefinition (self.Base)
+		if base then
+			valid, missing = base:CheckProvider (provider)
+		end
+	end
+	
+	missing = missing or {}
 	for k, _ in pairs (self.Methods) do
 		if not provider [k] then
 			missing [#missing + 1] = k
+			valid = false
 		end
 	end
-	return missing
+	return valid, missing
 end
 
 function self:GetName ()
 	return self.Name
 end
 
+function self:GetSingletonInterface ()
+	return self.Singleton
+end
+
 function self:RemoveMethod (name)
-	self.Methods [name] = false
+	self.Methods [name] = nil
+end
+
+function self:SetSingletonInterface (interface)
+	self.Singleton = interface
 end
